26511a386ff8c6728a40f5ffd6f280ec417f4a18
Mr.Jo <142702766+hoonjo123@users.noreply.github.com>
2024-04-08 22:36:09 +0900

    [feat] FCM 프론트 개발 (#6)

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,26 +2,35 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDq4aghoTHzLFT_9JISevOAwXTIe0VTy8g",
-    authDomain: "alioth-3a1ea.firebaseapp.com",
-    projectId: "alioth-3a1ea",
-    storageBucket: "alioth-3a1ea.appspot.com",
-    messagingSenderId: "320764927605",
-    appId: "1:320764927605:web:fde806e3a46e16f273a23c",
+    apiKey: "AIzaSyDotC7-wC0ldRBzZtd0sbebSdqoejVy5XQ",
+    authDomain: "aliothfcm.firebaseapp.com",
+    projectId: "aliothfcm",
+    storageBucket: "aliothfcm.appspot.com",
+    messagingSenderId: "288820360562",
+    appId: "1:288820360562:web:5af754e082f1bc75aa99eb",
+    measurementId: "G-B47NJH51VZ"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-    console.log(
-    '[firebase-messaging-sw.js] Received background message ',
-    payload
-    );
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/icon.png'
+        body: payload.notification.body,
+        icon: '/icon.png',
+        data: {
+            url: payload.data.url
+        }
     };
 
+    self.addEventListener('notificationclick', (event) => {
+        event.notification.close(); 
+        event.waitUntil(
+            clients.openWindow(burl+payload.data.url)
+        );
+    });
+    
     self.registration.showNotification(notificationTitle, notificationOptions);
 });
+
--- src/main.js
+++ src/main.js
@@ -1,61 +1,65 @@
-import { initializeApp } from 'firebase/app';
-import { getMessaging, getToken } from 'firebase/messaging';
-import { registerPlugins } from '@/plugins'
+import { createApp } from 'vue';
 import App from './App.vue';
-
-
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+import { registerPlugins } from '@/plugins';
 
 // Firebase 프로젝트 설정
 const firebaseConfig = {
-    apiKey: "AIzaSyDq4aghoTHzLFT_9JISevOAwXTIe0VTy8g",
-    authDomain: "alioth-3a1ea.firebaseapp.com",
-    projectId: "alioth-3a1ea",
-    storageBucket: "alioth-3a1ea.appspot.com",
-    messagingSenderId: "320764927605",
-    appId: "1:320764927605:web:fde806e3a46e16f273a23c",
-    measurementId: "G-J6V1GS14W5"
+    apiKey: "AIzaSyDotC7-wC0ldRBzZtd0sbebSdqoejVy5XQ",
+    authDomain: "aliothfcm.firebaseapp.com",
+    projectId: "aliothfcm",
+    storageBucket: "aliothfcm.appspot.com",
+    messagingSenderId: "288820360562",
+    appId: "1:288820360562:web:5af754e082f1bc75aa99eb",
+    measurementId: "G-B47NJH51VZ"
 };
 
 // Firebase 초기화
 const firebaseApp = initializeApp(firebaseConfig);
-
-// Firebase Messaging 서비스 인스턴스 초기화
 const messaging = getMessaging(firebaseApp);
+
+// 애플리케이션 인스턴스 생성
+const app = createApp(App);
+
+// 환경 변수를 전역 프로퍼티로 설정
+app.config.globalProperties.$apiBaseUrl = process.env.VUE_APP_API_BASE_URL;
+
+// 플러그인 등록
+registerPlugins(app);
 
 // 서비스 워커 등록
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/firebase-messaging-sw.js')
-            .then((registration) => {
-                console.log('서비스워커 등록 성공:', registration.scope);
-            }).catch((err) => {
-                console.log('서비스워크 등록 실패:', err);
-            });
-        });
-    }
+    navigator.serviceWorker.register('/firebase-messaging-sw.js').then(registration => {
+        console.log('Service Worker 등록 성공:', registration.scope);
+    }).catch(err => {
+        console.log('Service Worker 등록 실패:', err);
+    });
+}
 
-// Notification permission 요청 및 토큰 가져오기
-Notification.requestPermission().then((permission) => {
+// Notification 권한 요청 및 토큰 가져오기
+Notification.requestPermission().then(permission => {
     if (permission === 'granted') {
         console.log('알림 권한이 허용되었습니다.');
-
-        // 토큰 가져오기
-        getToken(messaging, { vapidKey: 'BMPifweL0hsWPF1VnGPjXL0S-myULnUgYoZCBhEd2bBNWaRcfegA6OlWd7ylNJq3wRz_cXlMwfcLZurfSVTPEXQ'
-            }).then((currentToken) => {
+        getToken(messaging, { vapidKey: 'BEx0hikggX8gLrvs5mw_M4ieqUpjQ_mFN19Y2hFaw8n5yKAJ6M4AeyHXsSOKo94FHSaXrns_dCXvMuZiz1AmkJI' }).then(currentToken => {
             if (currentToken) {
                 console.log('FCM 토큰:', currentToken);
             } else {
-                console.log('인스턴스 ID 토큰을 사용할 수 없습니다. 권한을 요청해 토큰을 생성하세요.');
+                console.log('인스턴스 ID 토큰을 사용할 수 없습니다.');
             }
-        }).catch((err) => {
-            console.log('토큰을 받아오는 중에 문제가 발생했습니다. ', err);
+        }).catch(err => {
+            console.log('토큰을 받아오는 중에 문제가 발생했습니다.', err);
         });
     } else {
         console.log('알림을 보낼 권한을 얻을 수 없습니다.');
     }
 });
 
+// 메시지 수신 대기
+onMessage(messaging, payload => {
+    console.log('Message received. ', payload);
+    alert(`새 메시지: ${payload.notification.title} - ${payload.notification.body}`);
+});
 
-const app = createApp(App);
-registerPlugins(app);
+// 애플리케이션 마운트
 app.mount('#app');
