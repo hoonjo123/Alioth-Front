9a56cbf17f4ae6947d991e67d5637c5c2f1e0b85
cherishjh <139551676+cherishjh@users.noreply.github.com>
2024-04-16 14:27:22 +0900

    [feat]: List Component 생성 : 계약목록 조회 적용 (#14)
    - detail 수정 중

---
+++ src/layouts/ListComponent.vue
@@ -0,0 +1,37 @@
+<template>
+
+    <v-data-table-virtual
+      :headers="columns"
+      :items="rows"
+      :items-length="10"
+      item-value="name"
+    ></v-data-table-virtual>
+</template>
+
+<script>
+export default {
+  props: {
+    columns: {
+      type: Array,
+      required: true
+    },
+    rows: {
+      type: Array,
+      required: true
+    }
+  },
+/*  methods: {
+    handleRowClick(row) {
+      this.$emit('row-click', row); // 이벤트를 발생시킵니다.
+    }
+  },
+  computed: {
+    getColumns() {
+      return this.columns;
+    },
+    getRows() {
+      return this.rows;
+    }
+  },*/
+};
+</script>
--- src/pages/contract/ContractListPage.vue
+++ src/pages/contract/ContractListPage.vue
@@ -3,53 +3,80 @@
   <v-main>
     <AppHeader></AppHeader>
       <v-divider></v-divider>
-      <h2>계약목록조회</h2>
-      <v-divider></v-divider>
-      <p><button @click="navigateToAddModify">계약 추가</button></p>
-      <v-data-table
-        :headers="headers"
-        :items="contracts"
-        class="elevation-1"
-        @click:row="navigateToDetail"
-      >
-      </v-data-table>
+      <v-card flat>
+        <v-card-title class="d-flex align-center pe-2">
+<!--          <v-icon icon="fa:fas fa-edit"></v-icon> &nbsp;-->
+          계약 목록
+          <v-spacer></v-spacer>
+          <v-text-field v-model="search" density="compact" label="Search" prepend-inner-icon="mdi-magnify" variant="solo-filled" flat hide-details single-line></v-text-field>
+          <v-row>
+            <v-col class="text-right">
+              <v-btn variant="outlined" @click="navigateToAddModify">계약 추가</v-btn>
+            </v-col>
+          </v-row>
+        </v-card-title>
+        <v-spacer></v-spacer>
+        <ListComponent :columns="tableColumns" :rows="tableRows"  @click:row="navigateToDetail" />
+      </v-card>
   </v-main>
 </template>
 
 <script>
 import AppSidebar from "@/layouts/AppSidebar.vue";
 import AppHeader from "@/layouts/AppHeader.vue";
-import { useRouter } from 'vue-router';
+import ListComponent from "@/layouts/ListComponent.vue";
+import { ref, onMounted } from 'vue'; // Composition API의 ref와 onMounted 임포트
+import axios from 'axios';
+import router from "@/router"; //확인
 
 export default {
-  components: { AppHeader, AppSidebar },
+  components: {AppSidebar, AppHeader, ListComponent},
   setup() {
-    const router = useRouter();
+    const tableColumns = [
+      {title: "No", key: "id"},
+      {title: "보험상품", key: "insuranceProductName"},
+      {title: "고객", key: "customName"},
+      {title: "계약 일자", key: "contractPeriod"},
+    ];
+    const tableRows = ref([]); // ref를 사용하여 반응형 데이터 생성
+
+
+    // 데이터 가져오는 함수
+    const fetchData = () => {
+      const baseUrl = process.env.VUE_APP_API_BASE_URL || 'http://localhost:8080'; // process.env를 사용하여 환경 변수에 접근
+      axios.get(`${baseUrl}/api/contract/list`)
+        .then(response => {
+          const data = response.data.result;
+          // 데이터를 가져온 후에 각 항목에 대한 ID를 추가합니다.
+          data.forEach((item, index) => {
+            item.id = index + 1; // 인덱스를 이용하여 ID를 할당합니다. 인덱스 + 1을 사용하여 1부터 시작하도록 합니다.
+          });
+          // tableRows에 데이터를 할당합니다.
+          tableRows.value = data;
+        })
+        .catch(error => {
+          console.log('Error fetching data:', error);
+        });
+    };
 
     function navigateToAddModify() {
+      console.log("Navigate to Add Modify");
       router.push('/ContractList/AddModify');
     }
-
     function navigateToDetail(item) {
       router.push({ path: '/ContractList/Detail', query: { id: item.id }});
     }
+    // 컴포넌트가 마운트되었을 때 데이터 가져오기
+    onMounted(() => {
+      fetchData();
+    });
 
+    // setup() 함수에서 반환해야 하는 객체
     return {
+      tableColumns,
+      tableRows,
       navigateToAddModify,
       navigateToDetail
-    }
-  },
-  data() {
-    return {
-      headers: [
-        { text: '보험상품', value: 'insuranceProduct' },
-        { text: '고객', value: 'customer' },
-        { text: '사원', value: 'employee' },
-        { text: '계약일자', value: 'contractDate' }
-      ],
-      contracts: [
-        { id: 1, insuranceProduct: '아무곳이나클릭해주세요보험', customer: '희준곤쥬', employee: '조사원', contractDate: '2023-01-01' }
-      ],
     };
   },
 };
