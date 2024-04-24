591aa31cd9d7efa33df54a683d63c7a4863b8d58
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2024-04-24 12:18:45 +0900

    Jgn/excel (#36)
    * [feat] : 계약 필터 수정
    
    * [fix] : 계약 리스트 필터 수정
    
    * [fix] : 계약 날짜 포멧 수정
    
    * [feat] : 엑셀 다운로드 필터 설정

--- src/pages/contract/ContractListPage.vue
+++ src/pages/contract/ContractListPage.vue
@@ -7,49 +7,56 @@
       <v-spacer></v-spacer>
       <v-row align="center">
         <v-col cols="4">
-          <v-text-field 
-            v-model="search" 
-            label="Search" 
-            prepend-inner-icon="mdi-magnify" 
-            variant="outlined" 
+          <v-text-field
+            v-model="search"
+            label="Search"
+            prepend-inner-icon="mdi-magnify"
+            variant="outlined"
             dense>
           </v-text-field>
         </v-col>
         <v-col class="d-flex justify-end">
-        <v-col cols="4" >
-          <v-select 
-            v-model="selectedStatus" 
-            :items="statusOptions" 
-            label="Status" 
-            outlined 
-            dense>
-          </v-select>
-        </v-col>
 
-        <v-col cols="4">
-          <v-select 
-            v-model="selectedCMmember" 
-            :items="salesMemberOptions" 
-            label="Sales Member" 
-            outlined 
-            dense>
-          </v-select>
+          <v-col cols="4">
+            <v-select
+              v-if="loginStore.memberRank==='HQ'"
+              v-model="selectedStatus"
+              :items="statusOptions"
+              item-title="key"
+              item-value="val"
+              label="Team Code"
+              outlined
+              dense>
+            </v-select>
+          </v-col>
+
+          <v-col cols="4">
+            <v-select
+              v-if="loginStore.memberRank==='MANAGER' || loginStore.memberRank==='HQ'"
+              v-model="selectedSMmember"
+              :items="salesMemberOptions"
+              label="Sales Member"
+              item-title="key"
+              item-value="val"
+              outlined
+              dense>
+            </v-select>
+          </v-col>
+
+          <v-col cols="1">
+            <v-btn
+              color="grey"
+              text
+              @click="navigateToAddModify">
+              계약추가
+            </v-btn>
+          </v-col>
         </v-col>
 
         <v-col cols="1">
-          <v-btn 
-            color="grey" 
-            text 
-            @click="navigateToAddModify">
-            계약추가
-          </v-btn>
-        </v-col>
-        </v-col>
-
-        <v-col cols="1">
-          <v-btn 
-            color="grey" 
-            text 
+          <v-btn
+            color="grey"
+            text
             @click="downloadExcel">
             엑셀다운로드
           </v-btn>
@@ -58,7 +65,7 @@
       <v-divider></v-divider>
       <v-spacer></v-spacer>
       <v-spacer></v-spacer>
-      <ListComponent :columns="tableColumns" :rows="tableRows" @row-click="navigateToDetail" />
+      <ListComponent :columns="tableColumns" :rows="tableRows" @row-click="navigateToDetail"/>
     </v-card>
   </v-main>
 </template>
@@ -68,92 +75,162 @@
 import AppSidebar from "@/layouts/AppSidebar.vue";
 import AppHeader from "@/layouts/AppHeader.vue";
 import ListComponent from "@/layouts/ListComponent.vue";
-import { ref, onMounted, watch } from 'vue';
+import {ref, onMounted, watch} from 'vue';
 import router from "@/router";
 import axiosInstance from '@/plugins/loginaxios';
+import {useLoginInfoStore} from "@/stores/loginInfo";
 
 export default {
-  components: { AppSidebar, AppHeader, ListComponent },
+  components: {AppSidebar, AppHeader, ListComponent},
   setup() {
+    const loginStore = useLoginInfoStore();
     const tableColumns = ref([
-      { title: "No", key: "id" },
-      { title: "보험상품", key: "insuranceProductName" },
-      { title: "고객", key: "customName" },
-      { title: "계약사원", key: "salesMemberName" },
-      { title: "계약 기간(년)", key: "contractPeriod" },
-      { title: '계약일자', key: 'contractDate' },
-      { title: '계약만료일자', key: 'contractExpireDate'},
-      { title: '계약상태', key: 'contractStatus' }
+      {title: "No", key: "id"},
+      {title: "보험상품", key: "insuranceProductName"},
+      {title: "고객", key: "customName"},
+      {title: "팀", key: "salesMemberResDto.teamName"},
+      {title: "계약사원", key: "salesMemberName"},
+      {title: "계약 기간(년)", key: "contractPeriod"},
+      {title: '계약일자', key: 'contractDate'},
+      {title: '계약만료일자', key: 'contractExpireDate'},
+      {title: '계약상태', key: 'contractStatus'}
     ]);
     const tableRows = ref([]);
     const search = ref('');
-    const selectedStatus = ref(null);
-    const statusOptions = ref(['New', 'Renewal', 'Cancellation']);
-    const selectedCMmember = ref(null);
-    const salesMemberOptions = ref([]);
+
+    const selectedStatus = ref(null)
+
+    let statusOptions = ref();
+
+    const selectedSMmember = ref(null);
+
+    let salesMemberOptions = ref();
 
     const fetchData = () => {
       const baseUrl = process.env.VUE_APP_API_BASE_URL || 'http://localhost:8080';
       axiosInstance.get(`${baseUrl}/api/contract/list`)
         .then(response => {
           let data = response.data.result;
-      console.log("Initial loaded data:", data);
+          statusOptions.value = [{key: "ALL", val: null}];
+          salesMemberOptions.value = [{key: "ALL", val: null}];
 
-      if (selectedStatus.value) {
-        data = data.filter(contract => contract.contractStatus === selectedStatus.value);
-      }
-      if (selectedCMmember.value) {
-        data = data.filter(contract => contract.salesMemberName === selectedCMmember.value);
-      }
+          if (useLoginInfoStore().memberRank === 'HQ') {
+            const newStatusOptions = response.data.result
+              .map(contract => ({
+                'key': contract.salesMemberResDto.teamName,
+                'val': contract.salesMemberResDto.teamCode
+              }));
+            const uniqueStatusOptions = Array.from(new Set(newStatusOptions.map(JSON.stringify))).map(JSON.parse);
+            statusOptions.value = [
+              ...statusOptions.value,
+              ...uniqueStatusOptions
+            ];
+            const newSalesMemberOptions = response.data.result
+              .filter(contract => contract.salesMemberResDto.teamCode === selectedStatus.value)
+              .map(contract => ({
+                'key': contract.salesMemberResDto.name,
+                'val': contract.salesMemberResDto.salesMemberCode
+              }))
+            const uniqueSalesMemberOptions = Array.from(new Set(newSalesMemberOptions.map(JSON.stringify))).map(JSON.parse);
 
-      tableRows.value = data.map((item, index) => ({ ...item, id: index + 1 }));
-      console.log("Filtered data:", tableRows.value);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
+            salesMemberOptions.value = [
+              ...salesMemberOptions.value,
+              ...uniqueSalesMemberOptions
+            ];
+          }
 
-      fetchSalesMembers();
+          if (useLoginInfoStore().memberRank === 'MANAGER') {
+            const newSalesMemberOptions = response.data.result
+              .filter(contract => contract.salesMemberResDto.teamCode === useLoginInfoStore().getmemberTeamCode)
+              .map(contract => ({
+                'key': contract.salesMemberResDto.name,
+                'val': contract.salesMemberResDto.salesMemberCode
+              }));
+            const uniqueSalesMemberOptions = Array.from(new Set(newSalesMemberOptions.map(JSON.stringify))).map(JSON.parse);
+
+            salesMemberOptions.value = [
+              ...salesMemberOptions.value,
+              ...uniqueSalesMemberOptions
+            ];
+
+            data = data.filter(contract => contract.salesMemberResDto.teamCode === useLoginInfoStore().getmemberTeamCode);
+          }
+
+          // console.log(response.data.result)
+          if(useLoginInfoStore().memberRank === 'FP'){
+            data = data.filter(contract => contract.salesMemberResDto.salesMemberCode === useLoginInfoStore().getMemberCode);
+          }
+
+          if (selectedStatus.value !== null) {
+            data = data.filter(contract => contract.salesMemberResDto.teamCode === selectedStatus.value);
+          }
+
+          if (selectedSMmember.value !== null) {
+            data = data.filter(contract => contract.salesMemberResDto.salesMemberCode === selectedSMmember.value);
+          }
+          tableRows.value = data.map((item, index) => ({
+            ...item,
+            id: index + 1,
+            contractDate: item.contractDate ? new Date(item.contractDate).toLocaleString() : 'N/A',
+            contractExpireDate: item.contractExpireDate ? new Date(item.contractExpireDate).toLocaleString() : 'N/A',
+          }));
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+
     };
 
-    
+
     const downloadExcel = () => {
-    const baseUrl = process.env.VUE_APP_API_BASE_URL || 'http://localhost:8080';
-    const requestData = {
-      startDate: null,
-      endDate: null
-    };
-    axiosInstance.post(`${baseUrl}/api/excel/export/contract`, requestData, {
-      responseType: 'blob',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => {
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', 'ContractList.xlsx');
-      document.body.appendChild(link);
-      link.click();
-      window.URL.revokeObjectURL(url);
-    })
-    .catch(error => {
-      console.error('Error downloading the file:', error.response);
-    });
-  };
-
-  
-    const fetchSalesMembers = () => {
       const baseUrl = process.env.VUE_APP_API_BASE_URL || 'http://localhost:8080';
-      axiosInstance.get(`${baseUrl}/api/members/list`)
+      const requestData = {
+        startDate: null,
+        endDate: null
+      };
+      let url = null
+      if (useLoginInfoStore().memberRank === 'HQ') {
+        if(selectedStatus.value === null && selectedSMmember.value === null){
+          url = `${baseUrl}/api/excel/export/contract`
+        }else if(selectedStatus.value !== null && selectedSMmember.value === null){
+          url = `${baseUrl}/api/excel/export/contract/${selectedStatus.value}`
+        }else if(selectedStatus.value !== null && selectedSMmember.value !== null){
+          url = `${baseUrl}/api/excel/export/contract/${selectedSMmember.value}`
+        }
+      }
+
+      if (useLoginInfoStore().memberRank === 'MANAGER') {
+        if(selectedSMmember.value === null){
+          url = `${baseUrl}/api/excel/export/contract`
+        }else {
+          url = `${baseUrl}/api/excel/export/contract/${selectedSMmember.value}`
+        }
+      }
+
+      if (useLoginInfoStore().memberRank === 'FP') {
+        url = `${baseUrl}/api/excel/export/contract`
+      }
+
+      console.log(selectedStatus.value)
+      console.log(selectedSMmember.value)
+      console.log(url)
+      axiosInstance.post(url, requestData, {
+        responseType: 'blob',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
         .then(response => {
-          salesMemberOptions.value = response.data.result.map(member => member.name);
-        //   if (salesMemberOptions.value.length > 0 && !selectedCMmember.value) {
-        //     selectedCMmember.value = salesMemberOptions.value[0]; // 첫 번째 멤버 이름을 기본 선택으로 설정
-        //   }
-        }).catch(error => {
-          console.error('Error fetching sales members:', error);
+          const url = window.URL.createObjectURL(new Blob([response.data]));
+          const link = document.createElement('a');
+          link.href = url;
+          link.setAttribute('download', 'ContractList.xlsx');
+          document.body.appendChild(link);
+          link.click();
+          window.URL.revokeObjectURL(url);
+        })
+        .catch(error => {
+          console.error('Error downloading the file:', error.response);
         });
     };
 
@@ -165,14 +242,19 @@ export default {
       router.push(`/ContractList/Detail/${item.item.id}`);
     };
 
-    watch([selectedStatus, selectedCMmember], fetchData);
+    watch(selectedStatus, () => {
+      selectedSMmember.value = null;
+      fetchData();
+    });
+
+    watch(selectedSMmember, () => {
+      fetchData();
+    });
 
     onMounted(() => {
       fetchData();
-      fetchSalesMembers();
     });
 
-    
 
     return {
       tableColumns,
@@ -180,10 +262,10 @@ export default {
       search,
       selectedStatus,
       statusOptions,
-      selectedCMmember,
+      selectedSMmember,
       salesMemberOptions,
       fetchData,
-      fetchSalesMembers,
+      loginStore,
       navigateToAddModify,
       navigateToDetail,
       downloadExcel
@@ -197,6 +279,7 @@ export default {
 .v-text-field, .v-select, .v-btn {
   height: 50px;
 }
+
 .d-flex.justify-end {
   display: flex;
   justify-content: flex-end;
